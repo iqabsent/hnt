3ffb6cc56090cf0c53c261b9259d36a00c649de7
IQAbsent <iqabsent@gmail.com>
2015-02-10 21:27:35 +0000

    Bit more clever with port ..

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var port = process.env.PORT || 8080;
 
 app.get('/', function(req, res){
   res.send("HELLO WORLD");
 });
 
-http.listen(8080, function(){
-  console.log('listening on *:8080');
+http.listen(port, function(){
+  console.log('listening on *:' + port');
 });
